7816e6bd1e9cab274b296a7c41fe38f54c21a4de
anish0m <mohsina.tabassum.rifa@gmail.com>
2025-10-18 16:03:42 +0600

    Initial project setup with requirements.txt

---
+++ interview_bot.py
@@ -0,0 +1,21 @@
+from typing import Dict, List, TypedDict
+from langgraph.graph import StateGraph, END
+
+
+class InterviewState(TypedDict):
+    # later
+    pass
+
+
+# Nodes
+#
+
+# Graph setup
+#
+
+# Test function
+#
+
+
+if __name__ == "__main__":
+    pass
