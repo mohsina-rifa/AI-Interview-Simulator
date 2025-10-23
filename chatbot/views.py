b799427b0e2141259b02b2a2950d457d44d793fd
mohsina-rifa <mohsina.rifa@brainstation-23.com>
2025-10-23 18:12:30 +0600

    web-app : Add chatbot URL routing and test view implementation

--- chatbot/views.py
+++ chatbot/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+import interview_bot
 
 # Create your views here.
+def test_view(request):
+    return JsonResponse({"success": True, "file": interview_bot.__file__})
