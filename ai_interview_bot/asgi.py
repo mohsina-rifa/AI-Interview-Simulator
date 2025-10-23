28d52a73504b2e736586eeb39c6096739203323c
mohsina-rifa <mohsina.rifa@brainstation-23.com>
2025-10-23 19:48:15 +0600

    web-app : Refactor ASGI configuration to support WebSocket routing and update chatbot URLs for interview handling

--- ai_interview_bot/asgi.py
+++ ai_interview_bot/asgi.py
@@ -10,7 +10,17 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import chatbot.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ai_interview_bot.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            chatbot.routing.websocket_urlpatterns
+        )
+    ),
+})
--- chatbot/urls.py
+++ chatbot/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.test_view, name='chatbot_test'),
+    path('start/', views.start_interview, name='start_interview'),
+    path('next/', views.next_question, name='next_question'),
+    path('answer/', views.submit_answer, name='submit_answer'),
 ]
