74feeebee4f6baea2e1859fe1f8f367cb3d90199
mohsina-rifa <mohsina.rifa@brainstation-23.com>
2025-10-24 11:25:45 +0600

    web-app : Add chat interface URL path to urlpatterns

--- chatbot/urls.py
+++ chatbot/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.chat_interface, name='chat_interface'),  # Add this line
     path('start/', views.start_interview, name='start_interview'),
     path('next/', views.next_question, name='next_question'),
     path('answer/', views.submit_answer, name='submit_answer'),
